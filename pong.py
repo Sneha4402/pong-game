d49cfe83e4f4cbec8b2060ed37ca993b602631ea
Sneka S <130648620+Sneha4402@users.noreply.github.com>
2025-05-17 14:09:33 +0530

    Add files via upload

---
+++ pong.py
@@ -0,0 +1,116 @@
+import turtle
+import winsound
+
+score_a = 0
+score_b =0
+screen = turtle.Screen()
+screen.setup(width = 800, height =600)
+screen.title("Pong Game")
+screen.bgcolor("dark blue")
+screen.tracer(0)
+
+#left paddle
+left_paddle = turtle.Turtle()
+left_paddle.speed(0)
+left_paddle.shape("square")
+left_paddle.color("white")
+left_paddle.shapesize(stretch_wid=5, stretch_len=1)
+left_paddle.penup()
+left_paddle.goto(-380,0)
+
+#right paddle
+right_paddle = turtle.Turtle()
+right_paddle.speed(0)
+right_paddle.shape("square")
+right_paddle.color("white")
+right_paddle.shapesize(stretch_wid=5, stretch_len=1)
+right_paddle.penup()
+right_paddle.goto(380,0)
+
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.dx = 0.25
+ball.dy = 0.25
+
+#Score
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0 Player B:0",align="center",font=("Ariel",24,"normal"))
+
+#moving_paddles
+def left_paddle_up():
+    left_paddle.sety(left_paddle.ycor()+20)
+
+def left_paddle_down():
+    left_paddle.sety(left_paddle.ycor()-20)
+
+def right_paddle_up():
+    right_paddle.sety(right_paddle.ycor()+20)
+
+def right_paddle_down():
+    right_paddle.sety(right_paddle.ycor()-20)
+
+screen.listen()
+screen.onkeypress(left_paddle_up,"w")
+screen.onkeypress(left_paddle_down,"s")
+screen.onkeypress(right_paddle_up,'Up')
+screen.onkeypress(right_paddle_down,"Down")
+
+
+while True:
+    screen.update()
+    #ball movement
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    #ball wall direction
+    #top wall
+    if ball.ycor()>290:
+        ball.sety(290)
+        ball.dy *= -1
+    #bottom wall
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *=-1
+    #right wall
+    if ball.xcor()> 390:
+        ball.setx(390)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Ariel", 24, "normal"))
+
+    #left wall
+    if ball.xcor() < -390:
+        ball.setx(-390)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B:{}".format(score_a,score_b), align="center", font=("Ariel", 24, "normal"))
+
+    # Game Over condition
+    if score_a == 10 or score_b == 10:
+        pen.goto(0, 0)
+        pen.write("Game Over", align="center", font=("Ariel", 24, "normal"))
+        break
+
+
+    #Collision with paddles:
+    if ball.xcor()> 370 and right_paddle.ycor()-50<ball.ycor()<right_paddle.ycor()+50:
+        ball.setx(360)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+    if ball.xcor() < -370 and left_paddle.ycor()-50<ball.ycor()<left_paddle.ycor()+50:
+        ball.setx(-360)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+
+
